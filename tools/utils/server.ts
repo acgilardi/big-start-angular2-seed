e12987ddb3012df234029444d4b62d74eefd602e
Anthony Gilardi <acgilardi@gmail.com>
2016-01-15 21:32:43 -0500

    Add port log

--- tools/utils/server.ts
+++ tools/utils/server.ts
@@ -3,6 +3,8 @@ import * as express from 'express';
 import * as tinylrFn from 'tiny-lr';
 import * as openResource from 'open';
 import * as serveStatic from 'serve-static';
+import * as util from 'gulp-util';
+import * as chalk from 'chalk';
 import {resolve} from 'path';
 import {APP_BASE, APP_DEST, DOCS_DEST, LIVE_RELOAD_PORT, DOCS_PORT, PORT} from '../config';
 
@@ -10,36 +12,37 @@ let tinylr = tinylrFn();
 
 
 export function serveSPA() {
-  let server = express();
-  tinylr.listen(LIVE_RELOAD_PORT);
+    let server = express();
+    tinylr.listen(LIVE_RELOAD_PORT);
 
-  server.use(
-    APP_BASE,
-    connectLivereload({ port: LIVE_RELOAD_PORT }),
-    express.static(process.cwd())
-  );
+    server.use(
+        APP_BASE,
+        connectLivereload({port: LIVE_RELOAD_PORT}),
+        express.static(process.cwd())
+    );
 
-  server.listen(process.env.PORT, () =>
-    openResource('http://localhost:' + PORT + APP_BASE + APP_DEST)
-  );
+    server.listen(process.env.PORT, function() {
+        util.log('Listening: ', chalk.yellow('port', process.env.PORT));
+        openResource('http://localhost:' + PORT + APP_BASE + APP_DEST);
+    });
 }
 
 export function notifyLiveReload(e) {
-  let fileName = e.path;
-  tinylr.changed({
-    body: { files: [fileName] }
-  });
+    let fileName = e.path;
+    tinylr.changed({
+        body: {files: [fileName]}
+    });
 }
 
 export function serveDocs() {
-  let server = express();
+    let server = express();
 
-   server.use(
-    APP_BASE,
-    serveStatic(resolve(process.cwd(), DOCS_DEST))
-  );
+    server.use(
+        APP_BASE,
+        serveStatic(resolve(process.cwd(), DOCS_DEST))
+    );
 
-   server.listen(DOCS_PORT, () =>
-    openResource('http://localhost:' + DOCS_PORT + APP_BASE)
-  );
+    server.listen(DOCS_PORT, () =>
+        openResource('http://localhost:' + DOCS_PORT + APP_BASE)
+    );
 }
